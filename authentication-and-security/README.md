dc9d358e4cb08ac1f9185d8ab93250699556a492
Tangsang2003 <077bei047.tangsang@pcampus.edu.np>
2025-01-13 20:41:26 +0545

    Add register and login functionality without encryption in Secrets

--- authentication-and-security/README.md
+++ authentication-and-security/README.md
@@ -1,1 +1,5 @@
 # 14. Authentication and Security
+
+## 14.1 Level 1 Security
+- Just saving customer's email and password in the database.
+- And validating when user's enter their credentials whether they match or not.
--- authentication-and-security/Secrets/app.js
+++ authentication-and-security/Secrets/app.js
@@ -2,28 +2,83 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 // app.use(bodyParser.urlencoded({
 //     extended: true
 // }));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+// USER SCHEMA
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+// USER MODEL
+const User = mongoose.model("User", userSchema);
+
+// ROUTES
+
 app.get("/", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 
-app.get("/login", (req, res)=> {
-    res.render("login");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get("/register", (req, res)=> {
-    res.render('register');
+app.post("/login", async(req, res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        const user = await User.findOne({email: username});
+        if (user) {
+            if (user.password === password) {
+                res.render("secrets");
+            }
+            else {
+                res.status(401).json({error: "Username and Passwords do not match!"});
+            }
+        }
+        else {
+            res.status(401).json({error: "User not found! Please register first."});
+        }
+
+    }catch(err) {
+        console.error(err);
+        res.status(500).json({message: err.message});
+    }
 });
 
+app.route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post(async (req, res) => {
+    try {
+      const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+      });
+      await newUser.save();
+      res.render("secrets");
 
-app.listen(3000, function(){
-    console.log("Server started at port 3000.");
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error! "});
+    }
+  });
+
+// app.get("/register", (req, res)=> {
+//     res.render('register');
+// });
+
+app.listen(3000, function () {
+  console.log("Server started at port 3000.");
 });
